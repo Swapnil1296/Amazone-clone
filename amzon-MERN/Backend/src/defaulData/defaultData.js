820e67a2ae1eda4cf8aa60aced5b8771916d14c9
Swapnil1296 <swapnillandage79@gmail.com>
2022-05-29 15:35:29 +0530

    few changes

--- amzon-MERN/Backend/src/defaulData/defaultData.js
+++ amzon-MERN/Backend/src/defaulData/defaultData.js
@@ -4,7 +4,7 @@ const productsdata = require("../constant/productdata");
 // storign productsdata into products collection manually without using postmen;
 const DefaultData = async () => {
   try {
-    await Products.deleteMany({});
+    await Products.deleteMany({}); // to avoid multiple data insert when save the page;
     const storeData = await Products.insertMany(productsdata);
     // console.log(storeData);
   } catch (error) {
--- amzon-MERN/Backend/src/index.js
+++ amzon-MERN/Backend/src/index.js
@@ -4,7 +4,12 @@ const mongoose = require("mongoose");
 const Db = require("./config/config");
 const Products = require("./models/product.schema");
 const DefaultData = require("./defaulData/defaultData");
+const cors = require("cors");
+const router = require("./Routes/routes");
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(router)
 
 const port = 8080;
 
@@ -13,4 +18,4 @@ app.listen(port, () => {
 });
 
 //callign default data which i have set in collection ;
-DefaultData();
+DefaultData(); // check atlas if it inserted succefully or not ;
